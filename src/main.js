561b02062fd7dbe0612dfabfea893b8f58a12df2
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-08-15 00:12:44 +0100

    Add close button for hamburger menu to improve mobile navigation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector("#nav-toggle");
 const navMenu = document.querySelector("#nav-menu");
+const toggleIcon = document.querySelector("#toggle-icon");
 
 navToggle.addEventListener("click", toggleNav);
 
@@ -8,8 +9,12 @@ function toggleNav() {
   if (navMenu.classList.contains("flex")) {
     navMenu.classList.remove("flex");
     navMenu.classList.add("hidden");
+     toggleIcon.classList.remove("fa-xmark");
+     toggleIcon.classList.add("fa-bars");
   } else {
     navMenu.classList.add("flex");
+    toggleIcon.classList.add("fa-xmark");
+    toggleIcon.classList.remove("fa-bars");
     navMenu.classList.remove("hidden");
   }
 }
