9f17c52c980310a010299cbaf3808f6fc3e8785e
Tolani Onajoko <omotolanionajoko@gmail.com>
2025-08-14 23:46:58 +0100

    Add nav-bar toggle functionality

--- src/main.js
+++ src/main.js
@@ -1,1 +1,15 @@
-const navToggle = document.querySelector('#nav-toggle');
+const navToggle = document.querySelector("#nav-toggle");
+const navMenu = document.querySelector("#nav-menu");
+
+navToggle.addEventListener("click", toggleNav);
+
+function toggleNav() {
+  // toggle between the two classes
+  if (navMenu.classList.contains("flex")) {
+    navMenu.classList.remove("flex");
+    navMenu.classList.add("hidden");
+  } else {
+    navMenu.classList.add("flex");
+    navMenu.classList.remove("hidden");
+  }
+}
